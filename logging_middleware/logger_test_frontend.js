86f2d978778369e0b557ebe514fef7d3a7e33989
NirmalMishra08 <145590866+NirmalMishra08@users.noreply.github.com>
2026-05-14 13:51:00 +0530

    ....

--- logging_middleware/logger_test_frontend.js
+++
@@ -1,7 +0,0 @@
-import { Log } from './logging_middleware/logger';
-
-function MyComponent() {
-  const handleClick = () => {
-    Log("frontend", "info", "component", "User clicked submit button");
-  };
-}
